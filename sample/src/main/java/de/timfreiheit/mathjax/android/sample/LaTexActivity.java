ffebde927c19630d4792a6ec1442766934b7af7c
Rajeef <rajeefmk@gmail.com>
2017-04-25 14:39:16 +0530

    Update sample app with example for listeners

--- sample/src/main/java/de/timfreiheit/mathjax/android/sample/LaTexActivity.java
+++ sample/src/main/java/de/timfreiheit/mathjax/android/sample/LaTexActivity.java
@@ -7,19 +7,17 @@ import android.support.annotation.NonNull;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import de.timfreiheit.mathjax.android.MathJaxView;
 
 /**
- *
  * Created by timfreiheit on 30.05.15.
  */
 public class LaTexActivity extends Activity
-        implements View.OnClickListener
-{
+        implements View.OnClickListener {
     MathJaxView mMathJaxView;
 
     private int exampleIndex = 0;
@@ -32,33 +30,36 @@ public class LaTexActivity extends Activity
         int id = v.getId();
 
         EditText e = (EditText) findViewById(R.id.edit);
-        switch (id){
+        switch (id) {
             case R.id.showBtn: {
-                mMathJaxView.setInputText(e.getText().toString());
+                showMathJax(e.getText().toString());
                 break;
             }
             case R.id.clearBtn: {
                 e.setText("");
-                mMathJaxView.setInputText("");
+                showMathJax("");
                 break;
             }
             case R.id.exampleBtn: {
                 e.setText(getExample(exampleIndex++));
-                if (exampleIndex > getResources().getStringArray(R.array.tex_examples).length-1)
-                    exampleIndex=0;
-                mMathJaxView.setInputText(e.getText().toString());
+                if (exampleIndex > getResources().getStringArray(R.array.tex_examples).length - 1)
+                    exampleIndex = 0;
+                showMathJax(e.getText().toString());
                 break;
             }
         }
     }
 
+    private void showMathJax(String value) {
+        mMathJaxView.setInputText(value);
+    }
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mMathJaxView = (MathJaxView) findViewById(R.id.laTexView);
-
+        setupMathJaxListener();
         EditText e = (EditText) findViewById(R.id.edit);
         e.setBackgroundColor(Color.LTGRAY);
         e.setTextColor(Color.BLACK);
@@ -76,6 +77,19 @@ public class LaTexActivity extends Activity
         t.setText(Html.fromHtml(t.getText().toString()));
 
 
+    }
+
+    private void setupMathJaxListener() {
+        mMathJaxView.setRenderListener(new MathJaxView.OnMathJaxRenderListener() {
+            @Override
+            public void onRendered() {
+                showToast();
+            }
+        });
+    }
+
+    private void showToast() {
+        Toast.makeText(this, "Render complete", Toast.LENGTH_SHORT).show();
     }
 }
 
